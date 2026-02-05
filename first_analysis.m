9cba1cf734e4571c29f31e1929542344534dc4ce
Faisal Jan <faisaljanfazal@gmail.com>
2026-02-05 10:13:54 +0100

    Add a scatterplot matrix in first_analysis.m

--- first_analysis.m
+++ first_analysis.m
@@ -10,6 +10,17 @@ columnNames = T.Properties.VariableNames;
 
 % Parallel coordinates
 parallelplot(T);
+title('Parallel Coordinates Plot of Wafer Properties');
 
 % Plot Description:
-% Goes Here
+% There are two wafers with same length and thickness 
+% but different widths.
+
+%% Scatter Plot Matrix
+% scatterplot matrix with univariate boxplot
+plotmatrix(T{:, :});
+hold on;
+
+% univariate boxplot
+boxplot(T{:, :}, 'Labels', columnNames, 'Color', [0.8 0.8 0.8]);
+hold off;
